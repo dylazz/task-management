f670db824bdc9c52ac95e67a3b50e94867812822
Dylan Azzopardi <dylan.azzopardi@betssongroup.com>
2025-06-20 17:51:06 +0200

    TaskItem validation

--- task-management-backend/task-management-backend/Models/TaskItem.cs
+++ task-management-backend/task-management-backend/Models/TaskItem.cs
@@ -1,14 +1,20 @@
-﻿using task_management_backend.Models.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using task_management_backend.Models.Enums;
 
 namespace task_management_backend.Models;
 
 public class TaskItem
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public int Id { get; init; }
+
+    [Required] 
+    [MaxLength(100)] 
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string Description { get; set; } = string.Empty;
     public Priority Priority { get; set; }
     public Status Status { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public DateTime CreatedDate { get; init; }
     
 }
