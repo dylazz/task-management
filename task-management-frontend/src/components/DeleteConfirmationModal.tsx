febda1bca8213802274f66c1ba22bd9400c9cc81
Dylan Azzopardi <dylan.azzopardi@betssongroup.com>
2025-06-23 23:23:50 +0200

    DeleteConfirmationModal style change
    Background changed to match TaskItemModal

--- task-management-frontend/src/components/DeleteConfirmationModal.tsx
+++ task-management-frontend/src/components/DeleteConfirmationModal.tsx
@@ -30,7 +30,7 @@ export const DeleteConfirmationModal = ({
 
     return (
         <div
-            className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+            className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50"
             onClick={onModalCancel} // Close when clicking outside the modal
         >
             <div
